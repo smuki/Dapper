178b6905a8c404befb96e7170a6f97f2055e387e
muki <sunriseyuen@qq.com>
2016-08-13 20:57:58 +0800

    -

--- src/Base64Coder.cs
+++ src/Base64Coder.cs
@@ -16,8 +16,8 @@ namespace Volte.Data.Dapper
         private char[] decosource;
         private char[] lookupTable = new char[64] {
             'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'
         };
 
         private int length, length2, length3;
@@ -96,20 +96,20 @@ namespace Volte.Data.Dapper
 
 
             switch (paddingCount) {
-                case 0:
-                    break;
+            case 0:
+                break;
 
-                case 1:
-                    result[blockCount * 4 - 1] = '=';
-                    break;
+            case 1:
+                result[blockCount * 4 - 1] = '=';
+                break;
 
-                case 2:
-                    result[blockCount * 4 - 1] = '=';
-                    result[blockCount * 4 - 2] = '=';
-                    break;
+            case 2:
+                result[blockCount * 4 - 1] = '=';
+                result[blockCount * 4 - 2] = '=';
+                break;
 
-                default:
-                    break;
+            default:
+                break;
             }
 
             return new string(result);
