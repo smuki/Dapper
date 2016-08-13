50788485202756f594f4c28bfdc45745f3c97233
muki <sunriseyuen@qq.com>
2016-08-13 23:12:39 +0800

    -

---
+++ src/Data/Streaming.cs
@@ -0,0 +1,187 @@
+using System;
+using System.IO;
+using System.Data;
+
+namespace Volte.Data.Dapper
+{
+
+    public abstract class Streaming {
+        const string ZFILE_NAME = "Streaming";
+        // Methods
+        public Streaming()
+        {
+            _QuotationMarksStart = "\"";
+            _QuotationMarksEnd   = "\"";
+            _vendor              = "MsSqlServer";
+            _connection          = null;
+            _transaction         = null;
+            _IsInTransaction     = false;
+            _cnnString           = "";
+        }
+
+        public Streaming(string name)
+        {
+            _QuotationMarksStart = "\"";
+            _QuotationMarksEnd   = "\"";
+            _vendor              = "MsSqlServer";
+            _connection          = null;
+            _transaction         = null;
+            _IsInTransaction     = false;
+            _cnnString           = "";
+            _DbName              = name;
+        }
+
+        public abstract DataTable AsDataTable(IDbCommand cmd, int top);
+        public abstract IDataReader GetDataReader(IDbCommand cmd, int top);
+
+        public void BeginTransaction()
+        {
+            if ((_connection == null) || (_connection.State == ConnectionState.Closed)) {
+                throw new DapperException("数据库未打开或未初始化！");
+            }
+
+            _transaction = _connection.BeginTransaction();
+            _IsInTransaction = true;
+
+        }
+
+        public void Close()
+        {
+            if (_connection.State == ConnectionState.Open) {
+                _connection.Close();
+            }
+
+            _IsInTransaction = false;
+        }
+
+        public void CommitTransaction()
+        {
+            if (_transaction == null) {
+                throw new DapperException("无可用事务！");
+            }
+
+            _transaction.Commit();
+            _IsInTransaction = false;
+        }
+
+        public int DoCommand(IDbCommand cmd)
+        {
+            if (_IsInTransaction) {
+                cmd.Transaction = _transaction;
+            }
+
+            cmd.Connection = _connection;
+            return cmd.ExecuteNonQuery();
+        }
+
+        public int DoSql(string sqlstring)
+        {
+            IDbCommand command1 = this.GetCommand();
+
+            if (_IsInTransaction) {
+                command1.Transaction = _transaction;
+            }
+
+            command1.Connection  = _connection;
+            command1.CommandText = sqlstring;
+
+            return command1.ExecuteNonQuery();
+        }
+
+        public abstract ExceptionTypes ErrorHandler(Exception e, out string message);
+        public abstract IDataAdapter GetAdapter(IDbCommand cmd);
+
+        public IDbCommand GetCommand()
+        {
+            return _connection.CreateCommand();
+        }
+
+        public abstract Streaming GetCopy();
+        public abstract DataRow GetDataRow(IDbCommand cmd);
+
+        public string GetQuotationColumn(string columnName)
+        {
+            return this.QuotationMarksStart + columnName + this.QuotationMarksEnd;
+        }
+
+        public abstract string GetStringParameter(string name, int i);
+
+        public virtual string TopWhere(string sqlString, int top)
+        {
+            return sqlString;
+        }
+
+        public abstract void Initialize(string connectionString);
+
+        public abstract int InsertRecord(IDbCommand cmd, out object identity);
+
+        public void Open()
+        {
+            try {
+                if (_connection.State == ConnectionState.Closed) {
+                    _connection.Open();
+                } else {
+                    //ZZDebug.Write(ZFILE_NAME,"Keep Open!...");
+                }
+
+            } catch (Exception exception1) {
+                ZZLogger.Error(ZFILE_NAME, exception1);
+                string _OcnnString = _cnnString;
+
+                if (_connection.State != ConnectionState.Closed) {
+                    _connection.Close();
+                }
+
+                _connection.ConnectionString = _OcnnString + ";Pooling=false;";
+                _connection.Open();
+            }
+
+        }
+
+        public void RollbackTransaction()
+        {
+            if (_transaction == null) {
+                //throw new DapperException("无可用事务！");
+            } else {
+                if (_IsInTransaction) {
+                    _transaction.Rollback();
+                }
+            }
+
+            _IsInTransaction = false;
+            _transaction = null;
+        }
+
+        public abstract SqlValueTypes SqlValueType(DbType type);
+
+        // Properties
+        public bool IsInTransaction
+        {
+            get {
+                return _IsInTransaction;
+            }
+        }
+
+        public string ConnectionString    { get { return _cnnString;           } set { _cnnString           = value; }  }
+        public string DbName              { get { return _DbName;              } set { _DbName              = value; }  }
+        public string QuotationMarksEnd   { get { return _QuotationMarksEnd;   } set { _QuotationMarksEnd   = value; }  }
+        public string QuotationMarksStart { get { return _QuotationMarksStart; } set { _QuotationMarksStart = value; }  }
+        public string Vendor              { get { return _vendor;              } set { _vendor              = value; }  }
+        public IDbConnection Connection   { get { return _connection;          } set { _connection          = value; }  }
+        public IDbTransaction Transaction { get { return _transaction;         } set { _transaction         = value; }  }
+        public string ParameterPrefix     { get { return _ParameterPrefix;     } set { _ParameterPrefix     = value; }  }
+        public string UnicodePrefix       { get { return _UnicodePrefix;       } set { _UnicodePrefix       = value; }  }
+
+        // Fields
+        private string _cnnString;
+        private bool _IsInTransaction;
+        private string _DbName;
+        private string _ParameterPrefix = "@";
+        private string _UnicodePrefix   = "";
+        private string _QuotationMarksEnd;
+        private string _QuotationMarksStart;
+        private string _vendor;
+        private IDbConnection _connection;
+        private IDbTransaction _transaction;
+    }
+}
---
+++ src/Data/XmlConfigLoader.cs
@@ -0,0 +1,260 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Xml;
+using System.IO;
+
+using Volte.Data.JsonObject;
+
+namespace Volte.Data.Dapper
+{
+    internal class XmlConfigLoader {
+        const string ZFILE_NAME = "XmlConfigLoader";
+
+        // Methods
+        private XmlConfigLoader()
+        {
+            _AttributeMappings = new Dictionary<string, AttributeMapping>();
+        }
+
+        private void AttributeMapping(XmlReader reader, string database, string clsMapping_Name, string cTable_Name, int colIndex)
+        {
+            string text1 = reader.GetAttribute("name");
+            string text2 = reader.GetAttribute("column");
+            string text3 = reader.GetAttribute("key");
+            string text5 = reader.GetAttribute("type");
+            string text6 = reader.GetAttribute("increment");
+            string text7 = reader.GetAttribute("timestamp");
+            string text8 = reader.GetAttribute("auto");
+
+            AttributeMapping map1 = null;
+
+            if (text1 != null) {
+                map1 = new AttributeMapping(text1);
+                map1.TableName = cTable_Name;
+
+                if (text2 != null) {
+                    map1.ColumnName = text2;
+
+                    if (text3 != null) {
+                        map1.PrimaryKey = true;
+                    }
+
+                    if (text5 == null) {
+                        string text9 = "?Ò«žX‚t‚t" + map1.ColumnName + "žX»³»³žX‚t?";
+                        throw new DapperException(text9, ExceptionTypes.XmlError);
+                    }
+
+                    switch (text5.ToLower()) {
+                    case "boolean": {
+                        map1.Type = DbType.Boolean;
+                        break;
+                    }
+
+                    case "bigint": {
+                        map1.Type = DbType.Int64;
+                        break;
+                    }
+
+                    case "binary": {
+                        map1.Type = DbType.Binary;
+                        break;
+                    }
+
+                    case "currency": {
+                        map1.Type = DbType.Currency;
+                        break;
+                    }
+
+                    case "date": {
+                        map1.Type = DbType.DateTime;
+                        break;
+                    }
+
+                    case "dbdate": {
+                        map1.Type = DbType.Date;
+                        break;
+                    }
+
+                    case "decimal": {
+                        map1.Type = DbType.Decimal;
+                        break;
+                    }
+
+                    case "double": {
+                        map1.Type = DbType.Double;
+                        break;
+                    }
+
+                    case "guid": {
+                        map1.Type = DbType.Guid;
+                        break;
+                    }
+
+                    case "object": {
+                        map1.Type = DbType.Object;
+                        break;
+                    }
+
+                    case "single": {
+                        map1.Type = DbType.Single;
+                        break;
+                    }
+
+                    case "smallint": {
+                        map1.Type = DbType.Int16;
+                        break;
+                    }
+
+                    case "tinyint": {
+                        map1.Type = DbType.Byte;
+                        break;
+                    }
+
+                    case "integer": {
+                        map1.Type = DbType.Int32;
+                        break;
+                    }
+
+                    case "varchar":
+                    case "string": {
+                        map1.Type = DbType.String;
+                        break;
+                    }
+
+                    default: {
+                        throw new DapperException("Ä¿‚tÈÔ?Ö§»³µÄ»³¾ÝžXÐÍ?", ExceptionTypes.XmlError);
+                    }
+                    }
+
+                    if ((text6 != null) && (text6.ToLower() == "true")) {
+                        map1.CanWrite = false;
+                        map1.AutoIdentity = true;
+                    }
+
+                    if ((text7 != null) && (text7.ToLower() == "true")) {
+                        map1.CanWrite = false;
+                        map1.Timestamp  = true;
+                    }
+
+                    if (text8 != null && (text8.ToLower() == "true")) {
+                        map1.CanWrite = false;
+                    }
+                }
+            }
+
+            if (map1 != null) {
+                lock (_PENDING) {
+                    _AttributeMappings[(database + "_" + clsMapping_Name + "_" + text1).ToLower()] = map1;
+                }
+            }
+
+            return;
+        }
+
+        private void GetClassMapping(XmlNodeReader node)
+        {
+            string text6;
+            string _classmapping_name = node.GetAttribute("name");
+            string text2     = node.GetAttribute("table");
+            string _database = node.GetAttribute("database");
+
+            int num1 = 0;
+            ClassMapping map1 = null;
+
+            if (((_classmapping_name != null) && (_database != null)) && (text2 != null)) {
+
+                map1 = new ClassMapping();
+                map1.TableName = text2;
+
+                int num2 = node.Depth;
+
+                while (node.Read() && (node.Depth > num2)) {
+                    if ((node.NodeType == XmlNodeType.Element) && (node.Name == "attribute")) {
+                        this.AttributeMapping(node, _database, _classmapping_name, text2, num1);
+                        num1++;
+                    }
+                }
+            } else {
+                text6 = "ClassMapping È±‚tclassName,DbName,tableName Ò«Ð©‚tÒª‚tÐÔ?";
+                throw new DapperException(text6, ExceptionTypes.XmlError);
+            }
+
+            return;
+        }
+
+        public static XmlConfigLoader Instance()
+        {
+            if (_xmlConfigLoader == null) {
+                _xmlConfigLoader = new XmlConfigLoader();
+            }
+
+            return _xmlConfigLoader;
+        }
+
+        internal void LoadClassMappingInfo(string xmlFile)
+        {
+            XmlDocument document1 = new XmlDocument();
+            string text1 = xmlFile;
+
+            try {
+
+                document1.Load(text1);
+
+                XmlNodeReader reader1 = new XmlNodeReader(document1);
+
+                while (reader1.Read()) {
+                    string text3;
+
+                    if (reader1.NodeType != XmlNodeType.Element) {
+                        continue;
+                    }
+
+                    if ((text3 = reader1.Name.ToLower()) == null) {
+                        continue;
+                    }
+
+                    text3 = string.IsInterned(text3);
+
+                    if (text3 != "class") {
+                        continue;
+                    }
+
+                    this.GetClassMapping(reader1);
+                    continue;
+                }
+
+                reader1.Close();
+            } catch (DapperException exception1) {
+                throw exception1;
+            } catch (Exception exception2) {
+                string text2 = "Ò«‚tžX»³‚t»³?" + text1 + "‚tžXÍËÒ«,Ò«žXÒ«?žX»³?Ò«‚t‚t»³‚t!";
+                text2 = text2 + exception2.Message;
+
+                ZZLogger.Error(ZFILE_NAME, exception2);
+
+                throw new DapperException(text2, ExceptionTypes.XmlError);
+            }
+        }
+
+        public void Clear()
+        {
+            _AttributeMappings = new Dictionary<string, AttributeMapping>();
+        }
+
+        // Properties
+
+        public Dictionary<string, AttributeMapping> AttributeMappings
+        {
+            get {
+                return _AttributeMappings;
+            }
+        }
+
+        // Fields
+        private Dictionary<string, AttributeMapping> _AttributeMappings;
+        private static readonly object _PENDING = new object();
+        private static XmlConfigLoader _xmlConfigLoader;
+    }
+}
