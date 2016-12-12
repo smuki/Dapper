280e1f4e63fdc8d957fbf62b15ef17518f1d7a16
sunrise <sunriseyuen@qq.com>
2016-12-12 20:23:51 +0800

    add cache writer

--- src/Util/ZZCache.cs
+++ src/Util/ZZCache.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO.Compression;
+using System.Threading;
 
 using Volte.Data.Json;
 
@@ -15,21 +16,21 @@ namespace Volte.Data.Dapper
 
         private readonly StringBuilder sb = new StringBuilder();
         private static ZZCache _Cache;
-        private static Queue<JSONArray> _que;
+        private static Queue<JSONTable> _que              = new Queue<JSONTable>();
         private static LRU<string, string> _cache         = new LRU<string, string>(5000);
         private static LRU<string, JSONTable> _cache_Cell = new LRU<string, JSONTable>(5000);
-        private static LRU<string, JSONArray> _JSONObject = new LRU<string, JSONArray>(5000);
+        private static LRU<string, JSONArray> _JSONObject = new LRU<string, JSONArray>(10);
         private static object _PENDING                    = new object();
         private static string _Cache_Location             = "";
+        private bool _Running                             = false;
+        private Thread _worker;
 
         public static ZZCache getInstance()
         {
             if (_Cache == null) {
-
-                Initialize();
-
-                _que   = new Queue<JSONArray>();
+                _que   = new Queue<JSONTable>();
                 _Cache = new ZZCache();
+                Initialize();
             }
 
             return _Cache;
@@ -37,6 +38,9 @@ namespace Volte.Data.Dapper
 
         internal ZZCache()
         {
+            _worker = new Thread(new ThreadStart(_Do_Worker));
+            _worker.IsBackground = true;
+            _worker.Start();
 
         }
 
@@ -99,25 +103,54 @@ namespace Volte.Data.Dapper
 
         public static void WriteJSONTable(string fileName, JSONTable _JSONTable)
         {
+            Initialize();
             _cache_Cell.SetValue(fileName, _JSONTable);
 
-            Initialize();
+            _que.Enqueue(_JSONTable);
 
-            try {
-                string _fileName = _Cache_Location + fileName + "T";
 
-                ZZLogger.Debug(ZFILE_NAME , _fileName);
+        }
 
-                using(FileStream fs = new FileStream(_fileName , FileMode.Create)) {
-                    BinaryFormatter formatter = new BinaryFormatter();
-                    GZipStream Compress = new GZipStream(fs, CompressionMode.Compress);
-                    formatter.Serialize(Compress, _JSONTable);
-                    Compress.Close();
+        internal void _Do_Worker()
+        {
+            if (_Running) {
+                return;
+            }
+
+            _Running  = true;
+
+            while (true) {
+
+                try {
+                    if (_que.Count > 0) {
+                        lock (_PENDING) {
+                            JSONTable _JSONTable= _que.Dequeue();
+
+                            try {
+                                string _fileName = _Cache_Location + _JSONTable.Variable.GetValue("lnk_DataUrl")+ "T";
+
+                                ZZLogger.Debug(ZFILE_NAME , _fileName);
+
+                                using(FileStream fs = new FileStream(_fileName , FileMode.Create)) {
+                                    BinaryFormatter formatter = new BinaryFormatter();
+                                    GZipStream Compress = new GZipStream(fs, CompressionMode.Compress);
+                                    formatter.Serialize(Compress, _JSONTable);
+                                    Compress.Close();
+                                }
+                            } catch (Exception ex) {
+                                ZZLogger.Debug(ZFILE_NAME, ex);
+                            }
+
+                        }
+                    }
+                } catch (Exception e) {
+
                 }
-            } catch (Exception ex) {
-                ZZLogger.Debug(ZFILE_NAME, ex);
+
+                Thread.Sleep(10000);
             }
         }
+
 
         public static JSONTable ReadJSONTable(string fileName)
         {
@@ -133,11 +166,13 @@ namespace Volte.Data.Dapper
 
                 if (File.Exists(_fileName)) {
                     using(FileStream fs = new FileStream(_fileName , FileMode.Open)) {
-                       GZipStream dStream = new GZipStream(fs, CompressionMode.Decompress, true);
+                        GZipStream dStream = new GZipStream(fs, CompressionMode.Decompress, true);
                         BinaryFormatter formatter = new BinaryFormatter();
                         _JSONTable = (JSONTable)formatter.Deserialize(dStream);
 
-                        _cache_Cell.SetValue(fileName, _JSONTable);
+                        if (_JSONTable.RecordCount<2000){
+                            _cache_Cell.SetValue(fileName, _JSONTable);
+                        }
                     }
                 } else {
                     _JSONTable = new JSONTable();
