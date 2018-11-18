b9c8dceb062a451de3cb143262f14e2884bc42ed
sunrise <sunriseyuen@qq.com>
2018-11-18 20:56:41 +0800

    -

--- Test2/p.bat
+++ Test2/p.bat
@@ -4,6 +4,7 @@ copy ..\dist\Volte.Data.Dapper.*
 
 %SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\csc mysql.cs /w:2 /debug+ /r:Volte.Data.Dapper.dll,Volte.Data.Json.dll
 %SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\csc mssql.cs /w:2 /debug+ /r:Volte.Data.Dapper.dll,Volte.Data.Json.dll,Volte.Utils.dll
+%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\csc pgsql.cs /w:2 /debug+ /r:Volte.Data.Dapper.dll,Volte.Data.Json.dll,Volte.Utils.dll
 
 
 %SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\csc Vertica.cs /w:2 /debug+ /r:Volte.Data.Dapper.dll,Volte.Data.Json.dll,Vertica.Data.dll
